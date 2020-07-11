def1223ac2ca68907c1c5e0770db74fd7ee54d05
AmericanEnglish <barajasc@umbc.edu>
2020-07-11 03:31:52 -0400

    maintain a user table

--- create_discord_tables.sql
+++ create_discord_tables.sql
@@ -1,6 +1,12 @@
-CREATE TABLE discord_table (
+CREATE TABLE discord_guild_table (
     guild_id TEXT,
     default_channel_id TEXT,
     timezone TEXT,
     PRIMARY KEY (guild_id)
 );
+
+CREATE TABLE discord_user_table (
+    user_id TEXT,
+    timezone TEXT,
+    PRIMARY KEY (user_id)
+)
